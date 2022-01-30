e718e4c1baae965e64f73aa2590a3340570a5483
Minh Hoang <minhhoangcap@gmail.com>
2022-01-31 00:37:24 +0700

    Add project files.

---
+++ Form_sort/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Form_sort
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Bubble_button = new System.Windows.Forms.Button();
+            this.Insert_button = new System.Windows.Forms.Button();
+            this.Bubble_panel = new System.Windows.Forms.Panel();
+            this.Insert_panel = new System.Windows.Forms.Panel();
+            
+            this.SuspendLayout();
+            // 
+            // Bubble_button
+            // 
+            this.Bubble_button.Location = new System.Drawing.Point(117, 56);
+            this.Bubble_button.Name = "Bubble_button";
+            this.Bubble_button.Size = new System.Drawing.Size(145, 29);
+            this.Bubble_button.TabIndex = 0;
+            this.Bubble_button.Text = "Bubble Sort";
+            this.Bubble_button.UseVisualStyleBackColor = true;
+            this.Bubble_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Insert_button
+            // 
+            this.Insert_button.Location = new System.Drawing.Point(544, 55);
+            this.Insert_button.Name = "Insert_button";
+            this.Insert_button.Size = new System.Drawing.Size(94, 29);
+            this.Insert_button.TabIndex = 1;
+            this.Insert_button.Text = "Insert Sort";
+            this.Insert_button.UseVisualStyleBackColor = true;
+            this.Insert_button.Click += new System.EventHandler(this.Insert_button_Click);
+            // 
+            // Bubble_panel
+            // 
+            this.Bubble_panel.Location = new System.Drawing.Point(30, 91);
+            this.Bubble_panel.Name = "Bubble_panel";
+            this.Bubble_panel.Size = new System.Drawing.Size(662, 125);
+            this.Bubble_panel.TabIndex = 2;
+            // 
+            // Insert_panel
+            // 
+            this.Insert_panel.Location = new System.Drawing.Point(30, 248);
+            this.Insert_panel.Name = "Insert_panel";
+            this.Insert_panel.Size = new System.Drawing.Size(662, 125);
+            this.Insert_panel.TabIndex = 3;
+            // 
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Insert_panel);
+            this.Controls.Add(this.Bubble_panel);
+            this.Controls.Add(this.Insert_button);
+            this.Controls.Add(this.Bubble_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Bubble_button;
+        private System.Windows.Forms.Button Insert_button;
+        private System.Windows.Forms.Panel Bubble_panel;
+        private System.Windows.Forms.Panel Insert_panel;
+       
+    }
+}
+
---
+++ Form_sort/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Form_sort
+{
+    public partial class Form1 : Form
+    {
+        Graphics bb_g;
+        Graphics ins_g;
+        bool bb_can;
+        bool ins_can;
+        //?? tạo mảng  random
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90, 50 };
+        public Form1()
+        {
+            InitializeComponent();
+            bb_can = false;
+        
+        }
+        public class Sort
+        {
+            public void vemang(int[] arr, Graphics g)
+            {
+                int n = arr.Length;
+                int w = 10;
+                for (int i = 0; i < n; i++)
+                {
+
+                    g.DrawRectangle(Pens.BlueViolet, i * 10, 0, w, arr[i]);
+                }
+            }
+
+        };
+        public class Bubble : Sort
+        {
+           
+            public void bb_sort(int[] arr, Graphics g)
+            {
+                int n = arr.Length;
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        if (arr[j] > arr[j + 1])
+                        {
+                            // swap temp and arr[i]
+                            int temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                        g.Clear(Color.White);
+                        
+                        vemang(arr, g);
+                        Thread.Sleep(1000);
+                        //timer1.Tick += timer1_Tick;
+
+                    }
+
+                }
+
+            }
+        }
+        public class Insert : Sort
+        {
+            public void ins_sort(int[] arr, Graphics g)
+            {
+                int n = arr.Length;
+                //dat thoi gian
+                //tu chay timer
+
+
+
+
+                for (int i = 1; i < n; ++i)
+                {
+                    int key = arr[i];
+                    int j = i - 1;
+
+                    // Move elements of arr[0..i-1],
+                    // that are greater than key,
+                    // to one position ahead of
+                    // their current position
+                    while (j >= 0 && arr[j] > key)
+                    {
+                        arr[j + 1] = arr[j];
+                        j = j - 1;
+                    }
+                    g.Clear(Color.White);
+                    vemang(arr, g);
+                    // goi tick de ve mang
+                    Thread.Sleep(1000);
+                    //timer1.Tick = vemang(arr, g);
+                    arr[j + 1] = key;
+                }
+            }
+        }
+        Bubble bb = new Bubble();
+        Insert ins = new Insert();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task t1 = new Task(
+                    () => {
+                        if (bb_can == true)
+                        {
+                            bb_g = Bubble_panel.CreateGraphics();
+                            ///tạo bubble sort
+                            bb.bb_sort(arr, bb_g);
+
+                            //bb_g.DrawLine(Pens.Black, 0, 0, 100, 100);
+                            bb_can = false;
+                        }
+                        else
+                        {
+                            // bb_g.Clear(Color.White);
+                            bb_can = true;
+                        }
+                    });
+            t1.Start();
+           
+        }
+
+        private void Insert_button_Click(object sender, EventArgs e)
+        {
+            Task t2 = new Task(
+                    () => {
+                        if (ins_can == true)
+                        {
+                            ins_g = Insert_panel.CreateGraphics();
+                            ///tạo bubble sort
+
+                            ins.ins_sort(arr, ins_g);
+                            //bb_g.DrawLine(Pens.Black, 0, 0, 100, 100);
+                            ins_can = false;
+                        }
+                        else
+                        {
+                            // ins_g.Clear(Color.White);
+                            ins_can = true;
+                        }
+
+                    }
+                    );
+            t2.Start();
+            
+        }
+
+        
+    }
+}
